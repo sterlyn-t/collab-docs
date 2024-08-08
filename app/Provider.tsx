806cef16472364c0a0c82fd061ffbc1e823f2a78
Sterlyn <sterlyntang31@gmail.com>
2024-08-08 11:24:34 -0400

    Chore: scaffolding user mentions

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -5,19 +5,30 @@ import {
   LiveblocksProvider,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 
 interface ProviderProps {
   children: React.ReactNode;
 }
 
 const Provider = (ProviderProps: ProviderProps) => {
+  const { user: clerkUser } = useUser();
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
       resolveUsers={async ({ userIds }) => {
         const users = await getClerkUsers({ userIds });
         return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+
+        return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>
